3fd64e2d228e6ef35a477babe66592255d633061
Tan Dung <tandung12072002@gmail.com>
2023-11-13 01:23:26 +0700

    rating

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Movie
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'calculate_ratings_count', 'calculate_ratings_avg']
+    readonly_fields = ['calculate_ratings_count', 'calculate_ratings_avg']
+
+admin.site.register(Movie, MovieAdmin) 
