1b9b72b24411728253d41268badb95ae7232d0c3
Tan Dung <tandung12072002@gmail.com>
2023-11-13 21:53:34 +0700

    celery

--- src/movies/tasks.py
+++ src/movies/tasks.py
@@ -1,6 +1,10 @@
+from celery import shared_task
+
 from .models import Movie
 
-def task_caculate_movie_rating(all=False, count=None):
+@shared_task(name="task_caculate_movie_ratings")
+def task_caculate_movie_ratings(all=False, count=None):
+
     qs = Movie.objects.needs_updating()
     if all:
         qs = Movie.objects.all()
