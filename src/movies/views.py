d03fd51b73c77c75b5c1c269e201ca195aa67d20
Tan Dung <tandung12072002@gmail.com>
2023-11-13 23:17:37 +0700

    view movies

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Movie
 
 # Create your views here.
+class MovieListView(generic.ListView):
+    template_name = 'movies/list.html'
+    paginate_by = 10
+    queryset = Movie.objects.all()
+
+movie_list = MovieListView.as_view()
+
+class MovieDetailView(generic.DetailView):
+    template_name = 'movies/detail.html'
+    queryset = Movie.objects.all()
+
+movie_detail = MovieDetailView.as_view()
