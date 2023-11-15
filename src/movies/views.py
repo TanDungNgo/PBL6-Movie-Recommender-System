a5a7ce4e827b70f8ccd7614762723d54e9c90954
Tan Dung <tandung12072002@gmail.com>
2023-11-15 16:49:47 +0700

    fix static base

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Avg
 
 from .models import Movie
 
@@ -7,12 +8,22 @@ from .models import Movie
 class MovieListView(generic.ListView):
     template_name = 'movies/list.html'
     paginate_by = 10
+    # context -> object_list
     queryset = Movie.objects.all().order_by('-rating_avg')
 
 movie_list_view = MovieListView.as_view()
 
 class MovieDetailView(generic.DetailView):
     template_name = 'movies/detail.html'
+    # context -> object -> id
     queryset = Movie.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        top_rated_movies = Movie.objects.all().order_by('-rating_avg')[:10]
+        
+        context['top_rated_movies'] = top_rated_movies
+        return context
 
 movie_detail_view = MovieDetailView.as_view()
