6de77e498d1cf8bb066869091187fa0b10a554ff
TrangLe <letrangle2002@gmail.com>
2023-11-17 15:57:02 +0700

    fix home

--- src/profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views  # Thay thế bằng tên module và view của bạn
-
-urlpatterns = [
-    path('signin/', views.signin, name='signin'),
-    path('signup/', views.signup, name='signup'),
-    path('signout/', views.signout, name='signout'),
-   
-]
