a110ff544f4566849e24eafe736efc634b126de9
Tan Dung <tandung12072002@gmail.com>
2023-11-18 20:47:12 +0700

    watch trailer

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from django.db.models import Avg
@@ -78,3 +79,21 @@ class Home(generic.ListView):
         return context
 
 home = Home.as_view()
+
+class MovieVideoView(generic.DetailView):
+    model = Movie
+    template_name = 'movies/watch_video.html'  # Tạo một template mới cho việc xem video
+    # context -> object -> id
+    queryset = Movie.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        movie_id = self.kwargs.get('pk')
+        url_movie = f'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={API_KEY}&language=en-US'
+        response = requests.get(url_movie)
+        response.raise_for_status()
+        data_movie = response.json()
+        context['key'] = data_movie.get("results", [])[0].get("key")
+        return context
+
+movie_video_view = MovieVideoView.as_view()
