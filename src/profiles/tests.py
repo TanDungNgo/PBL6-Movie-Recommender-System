c993dbbbd64ea751bf90ed2d0c1da0945666ccc5
TrangLe <letrangle2002@gmail.com>
2023-11-29 14:17:06 +0700

    fix

--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -685,7 +685,7 @@ class SignupFormTest(LiveServerTestCase):
 class UpdateProfileTest(LiveServerTestCase):
     def test_update_success(self):
         # Đường dẫn đến ChromeDriver
-        chromedriver_path = 'E:/PBL6/PBL6-Movie-Recommender-System/src/chromedriver.exe'
+        chromedriver_path = 'D:/Download/chromedriver-win64 (2)/chromedriver-win64/chromedriver.exe'
 
         # Khởi tạo driver Chrome với tùy chọn Service
         chrome_service = webdriver.chrome.service.Service(chromedriver_path)
@@ -791,7 +791,7 @@ class UpdateProfileTest(LiveServerTestCase):
 
     def test_update_invalid_username(self):
         # Đường dẫn đến ChromeDriver
-        chromedriver_path = 'E:/PBL6/PBL6-Movie-Recommender-System/src/chromedriver.exe'
+        chromedriver_path = 'D:/Download/chromedriver-win64 (2)/chromedriver-win64/chromedriver.exe'
 
         # Khởi tạo driver Chrome với tùy chọn Service
         chrome_service = webdriver.chrome.service.Service(chromedriver_path)
@@ -883,7 +883,7 @@ class UpdateProfileTest(LiveServerTestCase):
 
     def test_update_invalid_email(self):
         # Đường dẫn đến ChromeDriver
-        chromedriver_path = 'E:/PBL6/PBL6-Movie-Recommender-System/src/chromedriver.exe'
+        chromedriver_path = 'D:/Download/chromedriver-win64 (2)/chromedriver-win64/chromedriver.exe'
 
         # Khởi tạo driver Chrome với tùy chọn Service
         chrome_service = webdriver.chrome.service.Service(chromedriver_path)
