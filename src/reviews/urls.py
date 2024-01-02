6d09b484fe5a86634e25dc16885e0f14e9e86d32
Tan Dung <tandung12072002@gmail.com>
2024-01-02 21:28:34 +0700

    reply review

--- src/reviews/urls.py
+++ src/reviews/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('load-more-reviews/', views.load_more_reviews, name='load_more_reviews'),
     path('review/delete/', views.delete_review, name='delete_review'),
     path('review/update/', views.update_review, name='update_review'),
+    path('review/reply/', views.reply_review, name='reply_review'),
+    path('review/load_replies/', views.get_reply_reviews, name='get_reply_reviews')
 ]
